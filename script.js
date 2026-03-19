587460825dba79959e456ab668fbfce2a70b8b0a
Welepy-dev <mwsilva.contato@gmail.com>
2026-03-19 11:37:29 -0500

    viber

--- script.js
+++ script.js
@@ -4,18 +4,35 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight  // fixed typo
 
-// Move listener outside draw()
-const rects = []
-
 canvas.addEventListener("click", (e) => {
-    rects.push({ x: e.clientX - 4, y: e.clientY - 4 })
+  const x = e.clientX
+  const y = e.clientY
+
+  ctx.fillStyle = "black"
+  ctx.fillRect(x, y, 5, 5)
 })
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillRect(100, 100, 100, 100)
-    rects.forEach(r => ctx.fillRect(r.x, r.y, 10, 10))
-    requestAnimationFrame(draw)
-}
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.fillRect(100, 100, 100, 100)
+//     rects.forEach(r => ctx.fillRect(r.x, r.y, 10, 10))
+//     requestAnimationFrame(draw)
+// }
+//
+// draw()
 
-draw()
+// Move listener outside draw()
+// const rects = []
+//
+// canvas.addEventListener("click", (e) => {
+//     rects.push({ x: e.clientX - 4, y: e.clientY - 4 })
+// })
+//
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.fillRect(100, 100, 100, 100)
+//     rects.forEach(r => ctx.fillRect(r.x, r.y, 10, 10))
+//     requestAnimationFrame(draw)
+// }
+//
+// draw()
